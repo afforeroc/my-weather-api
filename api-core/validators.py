aaf00db856f21421d3e42b0b7d6b2635671d4bba
Andres Forero <afforeroc@unal.edu.co>
2020-09-15 00:04:51 -0500

    Validators functions have already json/application response

--- api-core/validators.py
+++ api-core/validators.py
@@ -3,7 +3,7 @@
 
 import re
 import logging
-from flask import abort
+from flask import make_response, jsonify, abort
 
 
 def check_emptiness(var_name, var_val):
@@ -11,11 +11,13 @@ def check_emptiness(var_name, var_val):
     if var_val is None:
         error_message = f" {var_name} is '{var_val}'."
         logging.error(error_message)
-        abort(500)
+        bad_response = make_response(jsonify(message=error_message), 500)
+        abort(bad_response)
     if var_val == '':
         error_message = f" {var_name} is '{var_val}' (empty)."
         logging.error(error_message)
-        abort(500)
+        bad_response = make_response(jsonify(message=error_message), 500)
+        abort(bad_response)
     sucess_message = f" {var_name}='{var_val}' was loaded: OK"
     logging.debug(sucess_message)
 
@@ -27,7 +29,8 @@ def check_regex(var_name, var, reg_expr):
     if pattern.fullmatch(var_str) is None:
         error_message = f" {var_name}:'{var_str}' doesn't match with '{reg_expr}' regex"
         logging.error(error_message)
-        abort(400)
+        bad_response = make_response(jsonify(message=error_message), 400)
+        abort(bad_response)
     else:
         sucess_message = f" {var_name}='{var}' matches with '{reg_expr}' regex: OK"
         logging.debug(sucess_message)
